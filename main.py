2e98f93524616cbbf4a102a00600604f854e8961
jtalb9727-rgb <jtalb9727@gmail.com>
2026-09-16 04:44:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import os
-import logging
 from telegram.ext import ApplicationBuilder, CommandHandler
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text('اهلاً! البوت اشتغل بنجاح 🎉')
+    await update.message.reply_text('البوت شغال ✅')
 
 if __name__ == '__main__':
-    print("PTB Version: 20.7")
-    print("البوت اشتغل...")
-    
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
