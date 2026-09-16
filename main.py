071c259b03b47eb466a93b12493079256a40bf88
jtalb9727-rgb <jtalb9727@gmail.com>
2026-09-17 02:03:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import os
-from telegram.ext import Updater, CommandHandler
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.environ.get('TOKEN')
 ADMIN_ID = int(os.environ.get('ADMIN_ID'))
 
-def start(update, context):
-    update.message.reply_text("البوت شغال على 13.15 ✅")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("البوت شغال على 20.7 ✅")
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
-    updater.start_polling()
-    updater.idle()
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
