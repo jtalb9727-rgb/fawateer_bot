9b2e468bd2c65a044f4f99b5b8c68d56f3e26311
jtalb9727-rgb <jtalb9727@gmail.com>
2026-09-16 05:01:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
-
 TOKEN = os.getenv("BOT_TOKEN")
-
 async def start(update, context):
     await update.message.reply_text('البوت شغال ✅')
-
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
