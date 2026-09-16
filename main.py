73e865c2812f81494721cbda869fd91c0768fd48
jtalb9727-rgb <jtalb9727@gmail.com>
2026-09-16 03:50:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,18 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# نقرأ التوكن من Variables
 TOKEN = os.environ.get("BOT_TOKEN")
 
-# دالة /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ البوت شغال! اهلا بك في بوت الفواتير")
 
 def main():
-    # نتأكد ان التوكن موجود
     if not TOKEN:
         print("BOT_TOKEN مش موجود!")
         return
     
-    # ننشئ التطبيق
     app = Application.builder().token(TOKEN).build()
-    
-    # نضيف امر /start
     app.add_handler(CommandHandler("start", start))
-    
-    # نشغل البوت
     print("البوت اشتغل...")
     app.run_polling()
 
