41946e8179657964463abfbc14bc67572512ff91
jtalb9727-rgb <jtalb9727@gmail.com>
2026-09-16 05:13:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler
-TOKEN = os.getenv("BOT_TOKEN")
-async def start(update, context):
-    await update.message.reply_text('البوت شغال ✅')
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.environ.get("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("البوت شغال يا جمال 🚀")
+
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    print("Bot is running...")
     app.run_polling()
